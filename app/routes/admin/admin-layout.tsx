4f61a89f4406b27fe8e456671d678b796e840f01
Bhavjot Singh <109903185+thebhavjotsingh@users.noreply.github.com>
2025-08-20 16:15:49 -0600

    Sign in routing done

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,27 @@
-import {Outlet} from "react-router";
+import {Outlet, redirect} from "react-router";
 import {MobileSidebar, NavItems} from "../../../components";
 import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import {account} from "~/appwrite/client";
+import {getExistingUser, storeUserData} from "~/appwrite/auth";
+
+export async function clientLoader() {
+    try {
+        const user = await account.get();
+
+        if (!user.$id) return redirect("/sign-in");
+
+        const existingUser = await getExistingUser(user.$id);
+
+        if (existingUser?.status === 'user') {
+            return redirect("/");
+        }
+
+        return existingUser?.$id ? existingUser: await storeUserData();
+    } catch (e) {
+        console.error('Error in clientLoader', e);
+        return redirect("/sign-in");
+    }
+}
 
 const AdminLayout = () => {
     return (
