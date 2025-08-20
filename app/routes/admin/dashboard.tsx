9ec652f74e936f4ab38bb5bba1ed9ff9e788fe94
Bhavjot Singh <109903185+thebhavjotsingh@users.noreply.github.com>
2025-08-19 23:32:31 -0600

    Basic UI done

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,8 @@
 import {Header, StatsCard, TripCard} from "../../../components";
+import { dashboardStats, user, allTrips } from "~/constants"
 
 const Dashboard = () => {
-    const user = {
-        name: "Singh"
-    };
-    const dashboardStats = {
-        totalUsers: 12450,
-        usersJoined: { currentMonth: 218, lastMonth: 176 },
-        totalTrips: 3210,
-        tripsCreated: { currentMonth: 150, lastMonth: 250 },
-        userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
-    }
+    const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
     return (
         <main className="dashboard wrapper">
@@ -23,25 +15,42 @@ const Dashboard = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
                     <StatsCard
                         headerTitle="Total Users"
-                        total={dashboardStats.totalUsers}
-                        currentMonthCount={dashboardStats.usersJoined.currentMonth}
-                        lastMonthCount={dashboardStats.usersJoined.lastMonth}
+                        total={totalUsers}
+                        currentMonthCount={usersJoined.currentMonth}
+                        lastMonthCount={usersJoined.lastMonth}
                     />
                     <StatsCard
                         headerTitle="Total Trips"
-                        total={dashboardStats.totalTrips}
-                        currentMonthCount={dashboardStats.tripsCreated.currentMonth}
-                        lastMonthCount={dashboardStats.tripsCreated.lastMonth}
+                        total={totalTrips}
+                        currentMonthCount={tripsCreated.currentMonth}
+                        lastMonthCount={tripsCreated.lastMonth}
                     />
                     <StatsCard
                         headerTitle="Active Users Today"
-                        total={dashboardStats.userRole.total}
-                        currentMonthCount={dashboardStats.userRole.currentMonth}
-                        lastMonthCount={dashboardStats.userRole.lastMonth}
+                        total={userRole.total}
+                        currentMonthCount={userRole.currentMonth}
+                        lastMonthCount={userRole.lastMonth}
                     />
                 </div>
             </section>
-            <TripCard />
+            <section className="container">
+                <h1 className="text-xl font-semibold text-dark-100">
+                    Created Trips
+                </h1>
+                <div className='trip-grid'>
+                    {allTrips.slice(0,4).map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+                        <TripCard
+                            key={id}
+                            id={id.toString()}
+                            name={name}
+                            imageUrl={imageUrls[0]}
+                            location={itinerary?.[0]?.location ?? ''}
+                            tags={tags}
+                            price={estimatedPrice}
+                        />
+                    ))}
+                </div>
+            </section>
         </main>
     )
 }
